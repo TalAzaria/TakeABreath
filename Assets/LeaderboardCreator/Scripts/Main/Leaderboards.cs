9ce624045ee857380b1eb362a72b4a806b9b8773
kfirsad <kfirsad@gmail.com>
2025-01-24 12:41:25 +0200

    Leaderboard

---
+++ Assets/LeaderboardCreator/Scripts/Main/Leaderboards.cs
@@ -0,0 +1,7 @@
+namespace Dan.Main
+{
+    public static class Leaderboards
+    {
+        public static LeaderboardReference TakeABreath = new LeaderboardReference("fc35a19b5efe01d9ec756a4726d5febbb51db4ee7a6cbd0d7f09a2a9559dfcc6");
+    }
+}
