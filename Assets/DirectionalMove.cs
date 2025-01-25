0d0b671a1f35f9d984f349919911a4e3b40f799b
TalAzaria <talazaria2@gmail.com>
2025-01-25 12:54:15 +0200

    d

--- Assets/DirectionalMove.cs
+++ Assets/DirectionalMove.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using UnityEngine;
+using UnityEngine.TestTools;
 
 public class DirectionalMove : MonoBehaviour
 {
@@ -10,30 +11,30 @@ public class DirectionalMove : MonoBehaviour
 
     private void Start()
     {
+        moveRate *= 5;
         originalPosition = transform.position;
         StartCoroutine(Move());
-        
     }
 
     private IEnumerator Move()
     {
         while (true)
         {
-            float targetLerpTime = Time.time + moveRate / 2;
+            float startingTime = Time.time;
+            float targetLerpTime = startingTime + moveRate;
             while (Time.time <= targetLerpTime)
             {
-                transform.position = Vector3.Lerp(transform.position, originalPosition + targetPositionOffset, Time.time);
+                transform.position = Vector3.Lerp(transform.position, originalPosition + targetPositionOffset, (Time.time - startingTime) / moveRate);
                 yield return 0;
             }
 
-            targetLerpTime = Time.time + moveRate / 2;
+            startingTime = Time.time;
+            targetLerpTime = startingTime + moveRate;
             while (Time.time <= targetLerpTime)
             {
-                transform.position = Vector3.Lerp(transform.position, originalPosition - targetPositionOffset, Time.time);
+                transform.position = Vector3.Lerp(transform.position, originalPosition, (Time.time - startingTime) / moveRate);
                 yield return 0;
             }
-
-            yield return 0;
         }
     }
 }
